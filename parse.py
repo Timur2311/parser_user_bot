c0122883f1b3a0e6c17a7946e11f62262efabee6
Timur2311 <zayniddinov.programming@gmail.com>
2023-09-11 20:47:12 +0500

    added some methods to get company director

--- parse.py
+++ parse.py
@@ -7,13 +7,25 @@ app = Client("my_account", api_id=api_id, api_hash=api_hash)
 
 
 def extract_company_and_director(text):
-    director = ""
-    text = re.sub("[^A-Za-z\s]", "", text)
-    company_name = ' '.join(text.split())
+    mchj_list = ["ooo","mchj","ооо","мчж","MChJ", "MCHJ","ООО","МЧЖ","OOO"]
+    company_permission = ""
+    
+    for sub_text in mchj_list:
+        if sub_text in text:
+            text = text.replace(sub_text,"")
+            company_permission = sub_text
+    company_text = re.sub("[^A-Za-z\s\-]", "", text)
+    
+    company_name = ' '.join(company_text.split())
+    if re.search("-$", company_name) !=None:
+        company_name = company_name.replace("-","")
+        
     print(company_name,"\n\n")
+    director = text.replace(company_name, "")
+    company_name+=f" {company_permission}"
     
-    
-    return director, company_name
+    # print(director,"\n\n")
+    return "director", company_name
     
 
 
